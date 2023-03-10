da95315441c6302e069a85983ad5ead8f833d475
Saeed Badran <salouri@gmail.com>
2023-03-10 07:51:40 +0400

    createAuction code

--- src/handlers/createAuction.js
+++ src/handlers/createAuction.js
@@ -1,9 +1,16 @@
 'use strict';
 
 async function createAuction(event, context) {
+  const body = JSON.parse(event.body);
+  const auction = {
+    title: body.title,
+    status: 'OPEN',
+    createdAt: new Date().toISOString(),
+  };
+
   return {
-    statusCode: 200,
-    body: JSON.stringify({ event, context }),
+    statusCode: 201,
+    body: JSON.stringify(auction),
   };
 }
 
