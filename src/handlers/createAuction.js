84f9ebeefb87b4d30d4c2d5100a9cc1f47b9c5e7
Saeed Badran <salouri@gmail.com>
2023-03-10 08:25:28 +0400

    add dynamodb table/code

--- src/handlers/createAuction.js
+++ src/handlers/createAuction.js
@@ -1,16 +1,30 @@
 'use strict';
+import crypto from 'crypto';
+import { DynamoDBClient, PutItemCommand } from '@aws-sdk/client-dynamodb';
+
+const client = new DynamoDBClient({ region: 'eu-west-1' });
 
 async function createAuction(event, context) {
   const body = JSON.parse(event.body);
+
   const auction = {
+    id: crypto.randomUUID(),
     title: body.title,
     status: 'OPEN',
     createdAt: new Date().toISOString(),
   };
 
+  const input = {
+    TableName: 'AuctionsTable',
+    Item: auction,
+  };
+
+  const command = new PutItemCommand(input);
+  const result = await client.send(command);
+
   return {
     statusCode: 201,
-    body: JSON.stringify(auction),
+    body: JSON.stringify(result),
   };
 }
 
