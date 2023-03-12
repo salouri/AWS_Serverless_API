2a2dfd2aab29a878cad0070c4afc6c267d99e2ed
Saeed Badran <salouri@gmail.com>
2023-03-12 06:31:25 +0400

    add middy and http-error package

--- src/handlers/createAuction.js
+++ src/handlers/createAuction.js
@@ -2,9 +2,14 @@
 import crypto from 'crypto';
 import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
 import { DynamoDBDocumentClient, PutCommand } from '@aws-sdk/lib-dynamodb';
+import middy from '@middy/core';
+import httpErrorHandler from '@middy/http-error-handler';
+import httpJsonBodyParser from '@middy/http-json-body-parser';
+import httpEventNormalizer from '@middy/http-event-normalizer';
+import createError from 'http-errors';
 
 const createAuction = async (event, context) => {
-  const body = JSON.parse(event.body);
+  const body = event.body;
 
   const auction = {
     id: crypto.randomUUID(),
@@ -24,14 +29,17 @@ const createAuction = async (event, context) => {
     result = await docClient.send(new PutCommand(params));
     console.log(`result: ${JSON.stringify(result, null, 2)}`);
   } catch (error) {
-    status = 500;
+    throw new createError.InternalServerError(error);
     console.log('error:', error);
   }
 
   return {
-    statusCode: status,
+    statusCode: 201,
     body: JSON.stringify(result),
   };
 };
 
-export const handler = createAuction;
+export const handler = middy(createAuction)
+  .use(httpJsonBodyParser())
+  .use(httpEventNormalizer())
+  .use(httpErrorHandler());
