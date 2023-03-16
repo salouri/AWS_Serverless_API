e68c32d9dad564fad108e8e8978907199617f792
Saeed Badran <salouri@gmail.com>
2023-03-16 11:17:11 +0400

    validate 'status' to only be OPEN or CLOSED

--- src/handlers/getAuctions.js
+++ src/handlers/getAuctions.js
@@ -1,29 +1,51 @@
 'use strict';
 import ddbDocClient from '../libs/ddbDocClient.js';
-import { ScanCommand } from '@aws-sdk/lib-dynamodb';
-import commonMiddleware from '../libs/commonMiddleware.js';
+import { QueryCommand } from '@aws-sdk/lib-dynamodb';
 import createError from 'http-errors';
+import commonMiddleware from '../libs/commonMiddleware.js';
+import validatorMiddleware from '@middy/validator';
+import getAuctionsSchema from '../libs/schemas/getAuctionsSchema.js';
+import { transpileSchema } from '@middy/validator/transpile';
 
 const getAuctions = async (event, context) => {
   let auctions;
+  const { status } = event.queryStringParameters;
 
   const params = {
     TableName: process.env.AUCTIONS_TABLE_NAME,
+    IndexName: 'statusAndEndDate',
+    KeyConditionExpression: '#status = :status',
+    ExpressionAttributeValues: {
+      // ':status': status || 'OPEN', // this is used when no validator used
+      ':status': status,
+    },
+    ExpressionAttributeNames: {
+      // this is needed because 'status' is a reserved word (replaces #status with 'status')
+      '#status': 'status',
+    },
   };
 
   try {
-    const result = await ddbDocClient.send(new ScanCommand(params));
-    console.log(`result: ${JSON.stringify(result, null, 2)}`);
-    auctions = result?.Items;
+    const data = await ddbDocClient.send(new QueryCommand(params));
+    console.log(`data: ${JSON.stringify(data, null, 2)}`);
+    auctions = data.count ? data?.Items : null; // without validator, this is needed
   } catch (error) {
     console.log(error);
     throw new createError.InternalServerError('Ops! something went wrong');
   }
 
   return {
-    statusCode: 200,
-    body: JSON.stringify(auctions),
+    statusCode: auctions ? 200 : 401,
+    body: JSON.stringify(auctions || 'No auctions found'),
   };
 };
 
-export const handler = commonMiddleware(getAuctions);
+export const handler = commonMiddleware(getAuctions).use(
+  validatorMiddleware({
+    eventSchema: transpileSchema(getAuctionsSchema),
+    ajvOptions: {
+      useDefaults: true,
+      strict: false,
+    },
+  })
+);
