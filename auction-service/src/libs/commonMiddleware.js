3790e88a3916187581d86a1013273ee41b75e4ab
Saeed Badran <salouri@gmail.com>
2023-03-20 09:35:28 +0400

    Auction: add CORS header on responses

--- auction-service/src/libs/commonMiddleware.js
+++ auction-service/src/libs/commonMiddleware.js
@@ -3,10 +3,12 @@ import httpHeaderNormalizer from '@middy/http-header-normalizer';
 import httpErrorHandler from '@middy/http-error-handler';
 import httpEventNormalizer from '@middy/http-event-normalizer';
 import httpJsonBodyParser from '@middy/http-json-body-parser';
+import cors from '@middy/http-cors';
 
 export default (fn) =>
   middy(fn)
     .use(httpHeaderNormalizer())
     .use(httpJsonBodyParser())
     .use(httpEventNormalizer())
-    .use(httpErrorHandler());
+    .use(httpErrorHandler())
+    .use(cors());
